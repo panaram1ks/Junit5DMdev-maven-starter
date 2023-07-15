6e544866adf84940f5ae226c12857312500c0bf9
evgen <parom.evgen@mail.ru>
2023-07-15 15:56:55 +0300

    init

---
+++ src/test/java/com/dmdev/junit/service/UserServiceTest.java
@@ -0,0 +1,12 @@
+package com.dmdev.junit.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTest {
+
+    @Test
+    void test() {
+        Assertions.assertTrue(false);
+    }
+}
